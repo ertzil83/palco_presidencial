9d44996d7b29bb6f256818df7f4d1bd402daf46e
ertzil83 <ertzil@gmail.com>
2025-02-21 09:35:48 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -255,10 +255,10 @@ function updateMatchInfoFromOpta()
     if(this.readyState === 4) {
       stats= JSON.parse(this.responseText);
       console.log(stats);
-      home_playerlist=stats.SoccerFeed.SoccerDocument[0].Team[0].Player;
-      home_teamInfo=stats.SoccerFeed.SoccerDocument[0].MatchData.TeamData[0];
-      away_playerlist=stats.SoccerFeed.SoccerDocument[0].Team[1].Player;
-      away_teamInfo=stats.SoccerFeed.SoccerDocument[0].MatchData.TeamData[1];
+      home_playerlist=stats.SoccerFeed.SoccerDocument.Team[0].Player;
+      home_teamInfo=stats.SoccerFeed.SoccerDocument.MatchData.TeamData[0];
+      away_playerlist=stats.SoccerFeed.SoccerDocument.Team[1].Player;
+      away_teamInfo=stats.SoccerFeed.SoccerDocument.MatchData.TeamData[1];
       teamInfo=home_teamInfo;
       playerlist=home_playerlist;
 	  sub_array=new Array();
